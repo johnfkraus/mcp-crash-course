8518b4cf4fa811391b290c327103835261e14829
John Kraus <johnkraus3@gmail.com>
2025-09-04 13:24:05 -0400

    Enhance main script with LLM integration and Stdio server parameters; update dependencies to include isort

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import asyncio
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 load_dotenv()
 # print(os.getenv("OPENAI_API_KEY"))
+
+llm = ChatOpenAI()  # whichever model
+
+stdio_server_parameters = StdioServerParameters(
+    command="python",
+    args=["/Users/blauerbock/workspaces/mcp/mcp-crash-course/servers/math_server.py"],
+    # math server
+)
+
 
 async def main():
     print("Hello from mcp-crash-course!")
