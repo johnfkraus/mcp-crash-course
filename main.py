19dc02088c8b2eae76cd0b907526d4b8d13a7053
John Kraus <johnkraus3@gmail.com>
2025-09-04 20:22:04 -0400

    Refactor main script to initialize session with Stdio client and integrate LLM agent for processing messages; added pprint for debugging.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import asyncio
 import os
+from pprint import pprint
 
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
@@ -19,10 +21,22 @@ stdio_server_parameters = StdioServerParameters(
     # math server
 )
 
-
 async def main():
-    print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_server_parameters) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Session initialized")
+            # tools = await session.list_tools()  # tools is an mcp object
+            tools = await load_mcp_tools(session)
+            # print(type(tools))
+            # for tool in tools:
+            #     pprint(tool, indent=2, depth=9, width=90)
 
+            agent = create_react_agent(llm, tools)
+            result = await agent.ainvoke({"messages": [HumanMessage("What is 54 + 2 * 3?")]})
+            print(result["messages"][-1].content)
+
+    # print("Hello from mcp-crash-course!")
 
 if __name__ == "__main__":
     asyncio.run(main())
